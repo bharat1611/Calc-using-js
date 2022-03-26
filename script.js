bd2f5a52c5d431334bd6e5b6b559dba3de100165
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2022-03-26 15:49:47 +0530

    Updated html, css and JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButton = document.querySelectorAll('[data-number]')
+const operationButton = document.querySelectorAll('[data-operation]')
+const equalsBotton = document.querySelector('[data-equals')
+const deleteButton = document.querySelector('[data-delete')
+const allClearButton = document.querySelector('[data-all-clear')
+const previousOperandTextElement = document.querySelector('[data-previous-operand')
+const currentOperandTextElement = document.querySelector('[data-current-operand')
+
